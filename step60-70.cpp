68bcfbabe65db83d1041bd429c34de515ce1f235
AndrewCollier123 <76977787+AndrewCollier123@users.noreply.github.com>
2023-05-06 13:53:52 -0700

    started 60-70

--- step60-70.cpp
+++ step60-70.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
+#include <fstream>
+#include <vector>   //requires -std=c++11
 
 int main()
 {
+    std::cout << "What is the name of the file you want to write to? (type: hello.txt)" << std::endl;
+    std::string filename;
+    std::cin >> filename;
 
+    std::ofstream file;     //can just add ("hello.txt"); after file
+    file.open(filename);     //can add (hello.txt, std::ios::app); to append any text when compiled to the current file instead of replacing it
+
+    if(file.is_open())  //checks if file is open
+    {
+        std::cout << "file is open" <<  std::endl;
+    }
+
+    file << "hello file" << std::endl;
+    std::vector<std::string> names;
+    names.push_back("bob");
+    names.push_back("joe");
+    names.push_back("guy");
+
+    for(std::string name : names)
+    {
+        file << name << std::endl;
+    }
+
+    file.close();   // this is unnecessary since it automatically does it for you
     return 0;
 }
